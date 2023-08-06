0648c2acb9446125b983e855f0f15b66613a133f
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-08-06 16:21:33 +0900

    added mqtt

---
+++ mqtt/README.md
@@ -0,0 +1,78 @@
+# MQTT Example   
+This is cc2500 and MQTT gateway application.   
+```
+            +----------+           +----------+           +----------+           +----------+
+            |          |           |          |           |          |           |          |
+            |Publisher |--(MQTT)-->|  Broker  |--(MQTT)-->|  ESP32   |--(SPI)--->|  cc2500  |==(Radio)==>
+            |          |           |          |           |          |           |          |
+            +----------+           +----------+           +----------+           +----------+
+
+            +----------+           +----------+           +----------+           +----------+
+            |          |           |          |           |          |           |          |
+==(Radio)==>|  cc2500  |--(SPI)--->|  ESP32   |--(MQTT)-->|  Broker  |--(MQTT)-->|Subscriber|
+            |          |           |          |           |          |           |          |
+            +----------+           +----------+           +----------+           +----------+
+```
+
+
+
+# Configuration
+![config-top](https://github.com/nopnop2002/esp-idf-cc2500/assets/6020549/c3805ad6-4584-45ff-810c-1eeb001af41a)
+![config-app](https://github.com/nopnop2002/esp-idf-cc2500/assets/6020549/823d7340-0069-47f8-821b-53bd36ec6099)
+
+
+## WiFi Setting
+
+![config-wifi](https://github.com/nopnop2002/esp-idf-cc2500/assets/6020549/ed00b25b-adc5-4ca7-b5d9-2cc763af0eed)
+
+## Radioi Setting
+
+### MQTT to Radio
+Subscribe with MQTT and send to Radio.   
+You can use mosquitto_pub as Publisher.   
+```sh ./mqtt_pub.sh```
+
+```
+            +----------+           +----------+           +----------+           +----------+
+            |          |           |          |           |          |           |          |
+            |Publisher |--(MQTT)-->|  Broker  |--(MQTT)-->|  ESP32   |--(SPI)--->|  cc2500  |==(Radio)==>
+            |          |           |          |           |          |           |          |
+            +----------+           +----------+           +----------+           +----------+
+```
+
+![config-radio-1](https://github.com/nopnop2002/esp-idf-cc2500/assets/6020549/a5592b19-735b-4352-939a-a756fba4b601)
+
+MQTT broker is specified by one of the following.   
+- IP address   
+ ```192.168.10.20```   
+- mDNS host name   
+ ```mqtt-broker.local```   
+- Fully Qualified Domain Name   
+ ```broker.emqx.io```
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\CC2500_receive   
+
+
+### Radio to MQTT
+Receive from Radio and publish as MQTT.   
+You can use mosquitto_sub as Subscriber.   
+```sh ./mqtt_sub.sh```
+
+```
+            +----------+           +----------+           +----------+           +----------+
+            |          |           |          |           |          |           |          |
+==(Radio)==>|  cc2500  |--(SPI)--->|  ESP32   |--(MQTT)-->|  Broker  |--(MQTT)-->|Subscriber|
+            |          |           |          |           |          |           |          |
+            +----------+           +----------+           +----------+           +----------+
+```
+
+![config-radio-2](https://github.com/nopnop2002/esp-idf-cc2500/assets/6020549/96639115-b5f2-4c36-9222-7773ede93c4e)
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\CC2500_transmitte   
+
+
+
---
+++ mqtt/main/main.c
@@ -0,0 +1,287 @@
+/* The example of CC250
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_sntp.h"
+#include "mdns.h"
+#include "esp_log.h"
+
+#include <cc2500.h>
+
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0))
+#define sntp_setoperatingmode esp_sntp_setoperatingmode
+#define sntp_setservername esp_sntp_setservername
+#define sntp_init esp_sntp_init
+#endif
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event */
+/* - are we connected to the AP with an IP? */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+#define TAG "MAIN"
+
+static int s_retry_num = 0;
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 256;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+void wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_ERROR_CHECK(esp_netif_init());
+
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_event_handler_instance_t instance_any_id;
+	esp_event_handler_instance_t instance_got_ip;
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&instance_any_id));
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+		IP_EVENT_STA_GOT_IP,
+		&event_handler,
+		NULL,
+		&instance_got_ip));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+	ESP_ERROR_CHECK(esp_wifi_start() );
+
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+		WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+		pdFALSE,
+		pdFALSE,
+		portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+	}
+
+	/* The event will not be processed after unregister */
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+	vEventGroupDelete(s_wifi_event_group);
+}
+
+esp_err_t query_mdns_host(const char * host_name, char *ip)
+{
+	ESP_LOGD(__FUNCTION__, "Query A: %s", host_name);
+
+	struct esp_ip4_addr addr;
+	addr.addr = 0;
+
+	esp_err_t err = mdns_query_a(host_name, 10000,	&addr);
+	if(err){
+		if(err == ESP_ERR_NOT_FOUND){
+			ESP_LOGW(__FUNCTION__, "%s: Host was not found!", esp_err_to_name(err));
+			return ESP_FAIL;
+		}
+		ESP_LOGE(__FUNCTION__, "Query Failed: %s", esp_err_to_name(err));
+		return ESP_FAIL;
+	}
+
+	ESP_LOGD(__FUNCTION__, "Query A: %s.local resolved to: " IPSTR, host_name, IP2STR(&addr));
+	sprintf(ip, IPSTR, IP2STR(&addr));
+	return ESP_OK;
+}
+
+void convert_mdns_host(char * from, char * to)
+{
+	ESP_LOGI(__FUNCTION__, "from=[%s]",from);
+	strcpy(to, from);
+	char *sp;
+	sp = strstr(from, ".local");
+	if (sp == NULL) return;
+
+	int _len = sp - from;
+	ESP_LOGD(__FUNCTION__, "_len=%d", _len);
+	char _from[128];
+	strcpy(_from, from);
+	_from[_len] = 0;
+	ESP_LOGI(__FUNCTION__, "_from=[%s]", _from);
+
+	char _ip[128];
+	esp_err_t ret = query_mdns_host(_from, _ip);
+	ESP_LOGI(__FUNCTION__, "query_mdns_host=%d _ip=[%s]", ret, _ip);
+	if (ret != ESP_OK) return;
+
+	strcpy(to, _ip);
+	ESP_LOGI(__FUNCTION__, "to=[%s]", to);
+}
+
+#if CONFIG_SENDER
+void tx_task(void *pvParameter)
+{
+	ESP_LOGI(pcTaskGetName(0), "Start");
+	uint8_t txBuf[64];
+
+	while (1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, txBuf, sizeof(txBuf), portMAX_DELAY);
+		ESP_LOGD(pcTaskGetName(0), "xMessageBufferReceive received=%d", received);
+		sendPacket(txBuf, received);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+
+	// never reach here
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void rx_task(void *pvParameter)
+{
+	ESP_LOGI(pcTaskGetName(0), "Start");
+	uint8_t rxBuf[64] = {0};
+	uint8_t rssi;
+	uint8_t lqi;
+
+	while(1) {
+		int rxLen = listenForPacket(rxBuf, 64, &rssi, &lqi);
+		if (rxLen) {
+			ESP_LOGI(pcTaskGetName(0), "rxLen=%d", rxLen);
+			ESP_LOGI(pcTaskGetName(0), "rxBuf=[%.*s]", rxLen, rxBuf);
+			int dbm;
+			if (rssi < 0x7F) {
+				dbm = (rssi/2) - 72;
+			} else {
+				dbm = abs(rssi-256);
+				dbm = (dbm/2) - 72;
+			}
+	
+			ESP_LOGI(pcTaskGetName(0), "RSSI(Raw): 0x%02x", rssi);
+			ESP_LOGI(pcTaskGetName(0), "RSSI(dBm): %d", dbm);
+			ESP_LOGI(pcTaskGetName(0), "LQI: 0x%02x", lqi);
+
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, rxBuf, rxLen, portMAX_DELAY);
+			if (sended == 0) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail");
+			}
+
+			
+		} // end if
+		vTaskDelay(1);
+	} // end while
+
+	// never reach here
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_RECEIVER
+
+void mqtt_sub(void *pvParameters);
+void mqtt_pub(void *pvParameters);
+
+void app_main()
+{
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Initialize WiFi
+	wifi_init_sta();
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Initialize mDNS
+	ESP_ERROR_CHECK( mdns_init() );
+
+	// Initialize CC2500
+	ret = begin(CONFIG_CC2500_CHANNEL);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "CC2500 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_SENDER
+	xTaskCreate(&tx_task, "TX", 1024*4, NULL, 5, NULL);
+	xTaskCreate(&mqtt_sub, "SUB", 1024*4, NULL, 2, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&rx_task, "RX", 1024*4, NULL, 1, NULL);
+	xTaskCreate(&mqtt_pub, "PUB", 1024*4, NULL, 2, NULL);
+#endif
+
+}
